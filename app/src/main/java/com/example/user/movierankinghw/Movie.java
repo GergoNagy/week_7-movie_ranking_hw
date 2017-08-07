3c9dac6a44527a5c24d0fbea9a8f235beda17578
Gergo Nagy <guzmop@gmail.com>
2017-08-07 15:21:09 +0100

    get/set added test ok

--- app/src/main/java/com/example/user/movierankinghw/Movie.java
+++ app/src/main/java/com/example/user/movierankinghw/Movie.java
@@ -5,4 +5,38 @@ package com.example.user.movierankinghw;
  */
 
 public class Movie {
+
+    private String title;
+    private String genre;
+    private int ranking;
+
+    public Movie(String title, String genre, int ranking){
+        this.title = title;
+        this.genre = genre;
+        this.ranking = ranking;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public int getRanking() {
+        return this.ranking;
+    }
+
+    public void addTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    public void addGenre(String newGenre) {
+        this.genre = newGenre;
+    }
+
+    public void addRanking(int newRanking) {
+        this.ranking = newRanking;
+    }
 }
--- app/src/test/java/com/example/user/movierankinghw/MovieTest.java
+++ app/src/test/java/com/example/user/movierankinghw/MovieTest.java
@@ -1,8 +1,55 @@
 package com.example.user.movierankinghw;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 07/08/2017.
  */
 
 public class MovieTest {
+
+    Movie movie;
+
+    @Before
+    public void before(){
+        movie = new Movie("Hobbit", "JRR Tolkin", 10);
+    }
+
+    @Test
+    public void testHasTitle(){
+        assertEquals("Hobbit", movie.getTitle());
+    }
+
+    @Test
+    public void testSetTitle(){
+        movie.addTitle("Lord of the rings");
+        assertEquals("Lord of the rings", movie.getTitle());
+
+    }
+
+    @Test
+    public void testHasGenre(){
+        assertEquals("JRR Tolkin", movie.getGenre());
+    }
+
+    @Test
+    public void testSetNewGenre(){
+        movie.addGenre("Bob");
+        assertEquals("Bob", movie.getGenre());
+    }
+
+    @Test
+    public void testHasRanking(){
+        assertEquals(10, movie.getRanking());
+    }
+
+    @Test
+    public void testNewRanking(){
+        movie.addRanking(9);
+        assertEquals(9, movie.getRanking());
+    }
+
 }
